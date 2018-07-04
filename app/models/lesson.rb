21df39c1068ccf6db4345b2a698b55aaceb6a548
guillaume <g.delory62@gmail.com>
2018-07-04 17:58:22 +0200

    model + validations

---
+++ app/models/lesson.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: lessons
+#
+#  id          :uuid             not null, primary key
+#  title       :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Lesson < ApplicationRecord
+	validates :title, length: { maximum: 50 }
+	validates :description, length: { maximum: 300 }
+end
