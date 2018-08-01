c13c8dca9350d7983ceb64fce478a60675b01b69
guillaume <g.delory62@gmail.com>
2018-08-01 21:25:06 +0200

    authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount_devise_token_auth_for 'User', at: 'auth'
   resources :lessons, except: %i[new edit]
 end
